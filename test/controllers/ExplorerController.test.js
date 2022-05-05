6d2960950e2040028dc0ab7fca31b308c4c3bc15
Marco Torres <mtorresm1401@alumno.ipn.mx>
2022-05-05 13:40:17 -0500

    Agregando prueba Controller

---
+++ test/controllers/ExplorerController.test.js
@@ -0,0 +1,31 @@
+const { TestScheduler } = require('jest')
+const ExplorerController = require('./../../lib/controllers/ExplorerController')
+
+describe("Prueba unitaria de ExplorerController", () =>{
+    test('Regresa los explorer de una mission', () => {
+        const explorersInNode = ExplorerController.getExplorersByMission("node");
+        
+        expect(explorersInNode).not.toBeUndefined();
+    })
+
+    test('2) Usuarios que su mision es node', () => {
+        const usernameInNode = ExplorerController.getExplorersUsernamesByMission('node')
+
+        expect(usernameInNode).toContain('ajolonauta1')
+        expect(usernameInNode).toContain('ajolonauta2')
+        expect(usernameInNode).toContain('ajolonauta3')
+        expect(usernameInNode).toContain('ajolonauta4')
+        expect(usernameInNode).toContain('ajolonauta5')
+        expect(usernameInNode).toContain('ajolonauta11')
+        expect(usernameInNode).toContain('ajolonauta12')
+        expect(usernameInNode).toContain('ajolonauta13')
+        expect(usernameInNode).toContain('ajolonauta14')
+        expect(usernameInNode).toContain('ajolonauta15')
+    })
+
+    test('3) Cantidad de explorers por mision elegida', () => {
+        const explorersAmmoutInNode = ExplorerController.getExplorersAmonutByMission("node");
+
+        expect(explorersAmmoutInNode).toBe(10);
+    })
+})
